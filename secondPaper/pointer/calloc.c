1e1756a4feec5c959e0c707c94ba0778d5cc20b2
hmjayed <jayedkn64@64gmail.com>
2020-03-26 13:34:29 +0600

    Pointer (dynamic memory) some code pratic in 26 march 2020

--- secondPaper/pointer/calloc.c
+++ secondPaper/pointer/calloc.c
@@ -1,25 +1,33 @@
-#include<stdio.h> // using malloc but not free memory
+#include<stdio.h>
 #include<stdlib.h>
 
 int main()
 {
     int *marks;
     int i, n;
-    printf("Please enter the number of students: ");
+    printf("Please enter the number of students: \n");
     scanf("%d", &n);
 
     // now allocate memory
-    marks = (int *)malloc(sizeof(int) * n);
+    marks = (int *) calloc(sizeof(int), n);
 
-    printf("Enter the marks for each student: \n");
+    if(marks == NULL) {
+        printf("Memory allocation failed for marks\n");
+        return 1;
+    }
+
+    printf("Enter marks for each student: \n");
     for(i = 0; i < n; i++) {
         scanf("%d", &marks[i]);
     }
+
     printf("Now here you can see the values:\n");
-    // Now print the marks array
     for(i = 0; i < n; i++) {
         printf("%d\n", marks[i]);
     }
+
+    // now free memory
+    free(marks);
 
     return 0;
 }
