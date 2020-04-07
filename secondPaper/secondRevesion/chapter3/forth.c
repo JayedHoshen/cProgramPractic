e9f8db89bf09f88c8c333f233f439989696af119
hmjayed <jayedkn64@64gmail.com>
2020-04-07 14:03:46 +0600

    update: 07-04-2020, secondRevision secondPaper

---
+++ secondPaper/secondRevesion/chapter3/forth.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp_in, *fp_out;
+    char *input_file = "in.txt";
+    char *output_file = "out.txt";
+    int num1, num2, sum;
+
+    fp_in = fopen(input_file, "r");
+    fp_out = fopen(output_file, "w");
+
+
+
+    return 0;
+}
