7ea2e31ec9706ffb088f0173e0e7359ad0efc846
hmjayed <jayedkn64@64gmail.com>
2020-05-05 18:35:11 +0600

    update: 05-05-2020, second revision first paper chapter 7 and 8

---
+++ firstPaper/secondRevision/chapter8/binary_search_fnc.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int b_search(int ara[], int low, int high, int key);
+
+int main()
+{
+    int ara[] = {1, 4, 6, 8, 9, 11, 14, 15, 20, 25, 33, 83, 87, 97, 99, 100};
+    int low = 0;
+    int high = 15;
+    int key = 97;
+
+    b_search(ara, low, high, key);
+
+    return 0;
+}
+
+int b_search(int ara[], int low, int high, int key)
+{
+    int mid;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if(key == ara[mid]) {
+            break;
+        }
+        if(key < ara[mid]) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+
+    if (low > high) {
+        printf("%d is not in the aray\n", key);
+    }
+    else {
+        printf("%d is found in the array. It is the %d th element of the array.\n", ara[mid], mid);
+    }
+}
