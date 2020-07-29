9139a1a0e394025891efddb6aa93b5d6fd88a76a
jayedhoshen <jayedkn64@gmail.com>
2020-07-29 16:40:02 +0600

    update

---
+++ firstPaper/thirdRevesion/chapter6/ara1.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // array declare but not used, that problem
+
+int main()
+{
+    int my_ara[10];
+
+    printf("%d\n", my_ara[0]);
+    printf("%d\n", my_ara[9]);
+
+    return 0;
+}
---
+++ firstPaper/thirdRevesion/chapter6/ara6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int my_ara[5] = {6, 7, 4, 6, 9};
+
+    printf("%d\n", my_ara[-1]);
+    printf("%d\n", my_ara[5]);
+    printf("%d\n", my_ara[100]);
+
+    return 0;
+}
