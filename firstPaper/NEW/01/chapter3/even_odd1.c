41ffd66c3a97597579d88d8600d8e1d48d5898ff
jayedhoshen <jayedkn64@gmail.com>
2021-08-03 09:12:39 +0600

    update: 3 August

---
+++ firstPaper/NEW/01/chapter3/even_odd1.c
@@ -0,0 +1,21 @@
+// even/odd number
+
+#include <stdio.h>
+
+int main()
+{
+    int number, remainder;
+
+    number = 5;
+
+    remainder = number % 2;
+
+    if (remainder == 0) {
+        printf("The number is even\n");
+    }
+    else {
+        printf("The number is odd\n");
+    }
+
+    return 0;
+}
